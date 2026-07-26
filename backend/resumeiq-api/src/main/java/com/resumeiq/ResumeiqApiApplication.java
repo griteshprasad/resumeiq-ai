53e2eec806fe2bed8c34870f3650144fe200a053
Gritesh  Prasad Kumhar <griteshprasad1@gmail.com>
2026-07-27 00:36:48 +0530

    chore: initialize Spring Boot backend with PostgreSQL

---
+++ backend/resumeiq-api/src/main/java/com/resumeiq/ResumeiqApiApplication.java
@@ -0,0 +1,13 @@
+package com.resumeiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeiqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeiqApiApplication.class, args);
+	}
+
+}
