3573b71e78b0d444fc85ebe8dcad801c71492bff
Gritesh  Prasad Kumhar <griteshprasad1@gmail.com>
2026-08-03 22:43:31 +0530

    fix: configure Angular Material theme and improve application shell

--- frontend/resumeiq-web/src/app/layout/components/header/header.ts
+++ frontend/resumeiq-web/src/app/layout/components/header/header.ts
@@ -1,13 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { AuthService } from '../../../features/auth/services/auth';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
@@ -15,6 +20,8 @@ import { AuthService } from '../../../features/auth/services/auth';
 export class Header {
 
   private readonly authService = inject(AuthService);
+
+  userName = 'Gritesh';
 
   logout(): void {
     this.authService.logout();
